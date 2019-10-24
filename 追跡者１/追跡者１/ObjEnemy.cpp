1b4feaba6d1df097a06e9969b241665d40169125
丈久 田中 <takenoko0510@outlook.jp>
2019-10-24 12:04:34 +0900

    敵

---
+++ 追跡者１/追跡者１/ObjEnemy.cpp
@@ -0,0 +1,23 @@
+#include"GameL/DrawTexture.h"
+#include"GameL/WinInputs.h"
+#include"GameL/SceneManager.h"
+
+#include"GameHead.h"
+#include"ObjEnemy.h"
+
+using namespace GameL;
+
+void CObjEnemy::Init()
+{
+	m_x = 100.0f;
+	m_y = 10.0f;
+	m_vx = 0.0f;
+	m_vy = 0.0f;
+}
+void CObjEnemy::Action()
+{
+}
+void CObjEnemy::Draw()
+{
+
+}
