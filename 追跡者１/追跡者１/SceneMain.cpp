8b912d60a7b4d10a0ee575137f2510c5fcb8e349
ryuya_m.dragon@icloud.com <ryuya_m.dragon@icloud.com>
2019-10-24 10:34:00 +0900

    うんこおおおおおおおおお

---
+++ 追跡者１/追跡者１/SceneMain.cpp
@@ -0,0 +1,26 @@
+//STLデバック機能をOFFにする
+#define _SECURE_SCL (0)
+#define _HAS_ITERATOR_DEBUGGING (0)
+
+#include "SceneMain.h"
+#include "GameHead.h"
+
+CSceneMain::CSceneMain()
+{
+
+}
+
+CSceneMain::~CSceneMain()
+{
+
+}
+
+void CSceneMain::InitScene()
+{
+
+}
+
+void CSceneMain::Scene()
+{
+
+}
