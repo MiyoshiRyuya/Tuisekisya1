f07fdd44867e98099b24b2756de1021a87008473
圭太 佐藤 <h1201919017@hiratagakuen.onmicrosoft.com>
2019-10-24 11:46:24 +0900

    stage

--- 追跡者１/追跡者１/objstage.cpp
+++ 追跡者１/追跡者１/objstage.cpp
@@ -38,5 +38,21 @@ void CObjstage::Action()
 //ÉhÉçÅ[
 void CObjstage::Draw()
 {
+	float c[4] = { 1.0f,1.0f,1.0f,1.0f };
+
+	RECT_F src;
+	RECT_F dst;
+
+	src.m_top = 0.0f;
+	src.m_left = 320.0f;
+	src.m_right = src.m_left + 64.0f;
+	src.m_bottom = 64.0;
+
+	dst.m_top = 0.0f;
+	dst.m_left = 0.0f;
+	dst.m_right = 64.0f;
+	dst.m_bottom = 64.0;
+
+	Draw::Draw(0, &src, &dst, c, 0.0f);
 
 }
