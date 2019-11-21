9f04c9a363b00fac2d71b55d3b1b44fcb7cb52dd
者 奏 <okakoshi@icloud.com>
2019-11-21 14:45:17 +0900

    asd

--- 追跡者１/追跡者１/ObjTo solve mystery.h
+++ 追跡者１/追跡者１/ObjTo solve mystery.h
@@ -16,10 +16,6 @@ public:
 	void Action();
 	void Draw();
 private:
-	float m_mou_x; //マウスの位置X 
-	float m_mou_y; //マウスの位置Y
-	bool  m_mou_r; //マウスの右ボタン
-	bool  m_mou_l; //マウスの左ボタン
 	float m_x;
 	float m_y;
 };
