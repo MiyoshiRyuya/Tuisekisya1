74093bf60b98ca5ffcebc7b172ee99608193e54e
丈久 田中 <takenoko0510@outlook.jp>
2019-11-29 12:36:56 +0900

    sa

--- 追跡者１/追跡者１/Map2.cpp
+++ 追跡者１/追跡者１/Map2.cpp
@@ -20,33 +20,32 @@ extern float genzaiti; //現在地
 //イニシャライズ
 void CObjMap2::Init()
 {
-	m_x = 300;
-	m_y = 100;
-	Hits::SetHitBox(this, m_x , m_y , 50, 60, ELEMENT_NULL, OBJ_MAP6, 2);
-	Hits::SetHitBox(this, m_x-200, m_y+10, 50, 60, ELEMENT_NULL, OBJ_MAP6, 2);
+	m_x = 0;
+	m_y = 350;
+	Hits::SetHitBox(this, m_x, m_y + 10, 60, 50, ELEMENT_GREEN, OBJ_MAP6, 2);
+	Hits::SetHitBox(this, m_x + 500, m_y, 50, 60, ELEMENT_GREEN, OBJ_MAP6, 2);
 
-	ax = 730;
-	ay = 530;
-	Hits::SetHitBox(this, ax , ay , 64, 64, ELEMENT_BLUE, OBJ_MAP6, 2);
+	ax = 500;
+	ay = 380;
+	Hits::SetHitBox(this, ax, ay, 64, 64, ELEMENT_BLUE, OBJ_MAP6, 2);
 
 	kx = 0;
 	ky = 200;
-	Hits::SetHitBox(this, kx, ky, 64, 64, ELEMENT_NULL, OBJ_MAP6, 2);
-	Hits::SetHitBox(this, kx-190, ky+10, 64, 64, ELEMENT_NULL, OBJ_MAP6, 2);
+	Hits::SetHitBox(this, kx, ky, 64, 64, ELEMENT_GREEN, OBJ_MAP6, 2);
+	Hits::SetHitBox(this, kx + 200, ky, 64, 64, ELEMENT_GREEN, OBJ_MAP6, 2);
+	Hits::SetHitBox(this, kx + 400, ky, 64, 64, ELEMENT_GREEN, OBJ_MAP6, 2);
+	Hits::SetHitBox(this, kx + 600, ky, 64, 64, ELEMENT_GREEN, OBJ_MAP6, 2);
+
+	bx = 0;
+	by = 0;
+	Hits::SetHitBox(this, bx, by, 800, 110, ELEMENT_GREEN, OBJ_MAP6, 1);
 
 }
 
 //アクション
 void CObjMap2::Action()
 {
-	CHitBox*hit = Hits::GetHitBox(this);
-	hit->SetPos(m_x , m_y );
-	CHitBox*hito = Hits::GetHitBox(this);
-	hito->SetPos(ax, ay);
-	CHitBox*hitoo = Hits::GetHitBox(this);
-	hito->SetPos(kx, ky);
 
-	genzaiti = 2;
 }
 
 //ドロー
@@ -67,12 +66,12 @@ void CObjMap2::Draw()
 	dst.m_right = 0.0f + m_x;
 	dst.m_bottom = 64.0f + m_y;
 
-	Draw::Draw(50, &src, &dst, c, 0.0f);
+	Draw::Draw(50, &src, &dst, c, 90.0f);
 
 	//椅子描画2
 	dst.m_top = 0.0f + m_y;
-	dst.m_left = 64.0f + 100;
-	dst.m_right = 0.0f + 100;
+	dst.m_left = 64.0f + 500;
+	dst.m_right = 0.0f + 500;
 	dst.m_bottom = 64.0f + m_y;
 
 	Draw::Draw(50, &src, &dst, c, 0.0f);
@@ -86,6 +85,7 @@ void CObjMap2::Draw()
 	Draw::Draw(51, &src, &dst, c, 0.0f);
 
 	//植木描画1
+
 	dst.m_top = 0.0f + ky;
 	dst.m_left = 64.0f + kx;
 	dst.m_right = 0.0f + kx;
@@ -95,26 +95,41 @@ void CObjMap2::Draw()
 
 	//植木描画2
 	dst.m_top = 0.0f + ky;
-	dst.m_left = 64.0f + 100;
-	dst.m_right = 0.0f + 100;
+	dst.m_left = 64.0f + 200;
+	dst.m_right = 0.0f + 200;
+	dst.m_bottom = 64.0f + ky;
+
+	Draw::Draw(52, &src, &dst, c, 0.0f);
+
+	//植木描画3
+	dst.m_top = 0.0f + ky;
+	dst.m_left = 64.0f + 400;
+	dst.m_right = 0.0f + 400;
+	dst.m_bottom = 64.0f + ky;
+
+	Draw::Draw(52, &src, &dst, c, 0.0f);
+
+	//植木描画4
+	dst.m_top = 0.0f + ky;
+	dst.m_left = 64.0f + 600;
+	dst.m_right = 0.0f + 600;
 	dst.m_bottom = 64.0f + ky;
 
 	Draw::Draw(52, &src, &dst, c, 0.0f);
 
 	//切り取り位置の設定
+	src.m_top = 0.0f;
+	src.m_left = 0.0f;
+	src.m_right = 728.0f;
+	src.m_bottom = 100.0f;
 
-src.m_top = 0.0f;
-src.m_left = 0.0f;
-src.m_right = 800.0f;
-src.m_bottom = 100.0f;
+	//表示位置の設定
+	dst.m_top = 0.0f;
+	dst.m_left = 0.0f; ;
+	dst.m_right = 800.0f;
+	dst.m_bottom = 110.0f;
 
-//表示位置の設定
-dst.m_top = 0.0f;
-dst.m_left = 0.0f;
-dst.m_right = 800.0f;
-dst.m_bottom = 110.0f;
-
-//描画
-Draw::Draw(9, &src, &dst, c, 0.0f);
+	//描画
+	Draw::Draw(9, &src, &dst, c, 0.0f);
 
 }
