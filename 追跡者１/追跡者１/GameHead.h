010ccd265898b7a9829e0753f6fef561b7819cf0
圭太 佐藤 <h1201919017@hiratagakuen.onmicrosoft.com>
2019-12-06 09:32:22 +0900

    lll

--- 追跡者１/追跡者１/GameHead.h
+++ 追跡者１/追跡者１/GameHead.h
@@ -252,5 +252,5 @@ extern float genzaiti; //現在地
 
 //シーンスタートクラス---------------------------
 //ゲーム開始時のシーンクラス登録
-#define SET_GAME_START  CSceneMap5
+#define SET_GAME_START  CSceneMap3
 //-----------------------------------------------
