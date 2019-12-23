f321d9cd929e41a92fd826870a697a9977f3bde5
わる <waru5679-touroku@yahoo.co.jp>
2019-12-23 10:25:11 +0900

    j

--- 追跡者１/追跡者１/ObjMenu2.cpp
+++ 追跡者１/追跡者１/ObjMenu2.cpp
@@ -354,7 +354,7 @@ void CObjMenu2::Draw()
 	}
 
 	else if (Cross == 1) {
-		Font::StrDraw(L"この十字架どっかで使えるしょ", 140, 200, 32, c);
+		Font::StrDraw(L"You'll be left as long as you have this", 140, 200, 32, c);
 	}
 	//カーソル（⇒）
 	if (GameOver == 0) {
